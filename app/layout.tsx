0818ec66587b6d159745c3004967f2cb3f2194d3
rayanmcheik <rayan.m.mcheik@gmail.com>
2025-10-14 14:39:00 +0300

    donee

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import './styles/globals.css';
+import '../styles/globals.css';
 import '../styles/custom.css';
 import type { ReactNode } from 'react';
 import Navbar from '../app/components/Navbar';
